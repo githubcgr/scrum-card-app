cb270a8090ecbf89f40682a9cd5bc12c26a815f1
Caique Galvanin Rodrigues <caique.rodrigues@picpay.com>
2019-11-16 10:43:51 -0300

    docker

--- backend/mongodb/db.js
+++ backend/mongodb/db.js
@@ -1,6 +1,6 @@
 module.exports = () => {
     const mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/scrumcards', {useNewUrlParser: true});
+    mongoose.connect('mongodb://172.17.0.1:27017/scrumcards', {useNewUrlParser: true});
 
     return mongoose;
 }
